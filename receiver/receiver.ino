cfb65de4f50ce58bed3fa5cdce7fff7f97a013dd
Igor <siebtel.ss@gmail.com>
2019-10-28 16:30:30 -0300

    consertando o hello world dos comunicadores

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,27 +1,31 @@
-    /*
-    * Arduino Wireless Communication Tutorial
-    *       Example 1 - Receiver Code
-    *                
-    * by Dejan Nedelkovski, www.HowToMechatronics.com
-    * 
-    * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-    */
-    #include <SPI.h>
-    #include <nRF24L01.h>
-    #include <RF24.h>
-    RF24 radio(7, 8); // CE, CSN
-    const byte address[6] = "00001";
-    void setup() {
-      Serial.begin(9600);
-      radio.begin();
-      radio.openReadingPipe(0, address);
-      radio.setPALevel(RF24_PA_MIN);
-      radio.startListening();
-    }
-    void loop() {
-      if (radio.available()) {
-        char text[32] = "";
-        radio.read(&text, sizeof(text));
-        Serial.println(text);
-      }
-    }
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN Pode ser qualquer porta digital
+const byte endereco[][10] = {"1arduino", "2arduino"};
+void setup() {
+  Serial.begin(9600);
+
+
+  //define o pipe de escrita
+  radio.openWritingPipe(endereco[0]);
+  //define o pipe de leitura
+  radio.openReadingPipe(1, endereco[1]);
+
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    const char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,26 +1,34 @@
 //https://howtomechatronics.com/tutorials/arduino/arduino-wireless-communication-nrf24l01-tutorial/
 
-    /*
-    * Arduino Wireless Communication Tutorial
-    *     Example 1 - Transmitter Code
-    *                
-    * by Dejan Nedelkovski, www.HowToMechatronics.com
-    * 
-    * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-    */
-    #include <SPI.h>
-    #include <nRF24L01.h>
-    #include <RF24.h>
-    RF24 radio(7, 8); // CE, CSN
-    const byte address[6] = "00001";
-    void setup() {
-      radio.begin();
-      radio.openWritingPipe(address);
-      radio.setPALevel(RF24_PA_MIN);
-      radio.stopListening();
-    }
-    void loop() {
-      const char text[] = "Hello World";
-      radio.write(&text, sizeof(text));
-      delay(1000);
-    }
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 1 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN Pode ser qualquer porta digital
+//definindo os pipes
+const byte endereco[][10] = {"2arduino", "1arduino"};
+
+void setup() {
+  //define o pipe de escrita
+  radio.openWritingPipe(endereco[0]);
+  //define o pipe de leitura
+  radio.openReadingPipe(1, endereco[1]);
+
+  radio.startListening();
+}
+void loop() {
+  //precisa parar de ouvir para poder enviar
+  radio.stopListening();
+  const char text[32] = "Hello World";
+  radio.write(&text, sizeof(text));
+  radio.startListening();
+
+  delay(1000);
+}
