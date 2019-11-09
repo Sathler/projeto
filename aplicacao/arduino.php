c65f2444d651b9ead810c9aa34e4befd11b567b2
Vinicius Sathler <vinicius_sathler3@yahoo.com.br>
2019-11-09 15:42:17 -0300

    unrefined functional application

--- aplicacao/arduino.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once 'crud.php';
-    session_start();
-    var_dump($_GET);
-    $x = cadastrar($_GET["arduino_id"], $_GET["arduino_nome"], $_GET["arduino_h"], $_GET["arduino_w"], $_GET["arduino_s"]);
-    var_dump($x);
-?>
