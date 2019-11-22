916a08fc170eda9304154af33f26db2ae8db324c
Vinicius Sathler <Vinicius.sathler2@gmail.com>
2019-11-22 09:41:58 -0300

    limpa

--- trbalho bd1/README.md
+++
@@ -1,9 +0,0 @@
-# Aviso
-
-Para usar as funções de administrador, entre
-
-```bash
->> login
-Nome de usuário: admin
-Senha: admin
-```
--- trbalho bd1/runner.py
+++
@@ -1,4 +0,0 @@
-from anime_shell.anime_shell import AnimeShell
-from anime_shell.anime_shell import ListaAnimes
-
-AnimeShell().executar()
